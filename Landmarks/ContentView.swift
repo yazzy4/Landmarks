f6e4bf9664e62e77590638bec554dc35ef542e4d
yazzy4 <yazzy4@gmail.com>
2019-10-09 15:53:50 -0400

    first commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Yaz Burrell on 10/1/19.
+//  Copyright © 2019 Yaz Burrell. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello New Swift")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
